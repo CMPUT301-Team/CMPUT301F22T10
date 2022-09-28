b25e8001fa9e49e47e149b57223e256f860a68c9
Wanlin Zheng <wanlin1@ualberta.ca>
2022-09-27 19:56:59 -0600

    added abstract shape class

---
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+public abstract class Shape {
+    private Integer x;
+    private Integer y;
+
+    protected Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+}
