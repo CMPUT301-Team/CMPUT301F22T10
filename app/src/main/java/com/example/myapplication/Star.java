696496ffdaf2c4ddc1ff4255334c83bb48585ab4
Joshua Vuong <jtvuong@ualberta.ca>
2022-10-01 15:18:02 -0600

    Added Star class

---
+++ app/src/main/java/com/example/myapplication/Star.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+public class Star extends Shape{
+    private Integer x;
+    private Integer y;
+
+    private Star(Integer x, Integer y) {
+        super(x,y);
+    }
+}
