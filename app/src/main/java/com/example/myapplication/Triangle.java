993707642ed6b94fceaef2381db9745643d56cec
Dylan Clarke <dylanclarke777@gmail.com>
2022-10-02 16:28:56 -0600

    color blue

--- app/src/main/java/com/example/myapplication/Triangle.java
+++ app/src/main/java/com/example/myapplication/Triangle.java
@@ -5,7 +5,10 @@ public class Triangle extends Shape{
     private int x;
     private int y;
 
+
     public Triangle(Integer x, Integer y, int x1, int y1) {
         super(x, y);
+        String color = "blue";
+
     }
 }
