a959eec252870af28238579688bc18634ccb8d57
Dylan Clarke <dylanclarke777@gmail.com>
2022-10-02 16:10:20 -0600

    Added Triangle

---
+++ app/src/main/java/com/example/myapplication/Triangle.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public class Triangle extends Shape{
+
+    private int x;
+    private int y;
+
+    public Triangle(Integer x, Integer y, int x1, int y1) {
+        super(x, y);
+    }
+}
