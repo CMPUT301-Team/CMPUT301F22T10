8904b5167ac8ee9ec31f720689e787c59780f86c
Joshua Vuong <jtvuong@ualberta.ca>
2022-10-02 16:48:37 -0600

    Added a Dodecahedron class

--- app/src/main/java/com/example/myapplication/Dodecahedron.java
+++ app/src/main/java/com/example/myapplication/Dodecahedron.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 public class Dodecahedron extends Shape{
 
+    private String color = "purple";
+
     private Dodecahedron(Integer x, Integer y) {
         super(x,y);
     }
