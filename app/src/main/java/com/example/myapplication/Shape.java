a2375d05555f7a8f387490ac9d0a11f441c8fbe3
micheal-chow <114542853+micheal-chow@users.noreply.github.com>
2022-10-02 17:26:21 -0600

    Update Shape.java

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
     
-    String color = "cerulean";
+    
 
     protected Shape(Integer x, Integer y) {
         this.x = x;
