b0c0f8d508c979542b80a775a1dba111275a8706
Joshua Vuong <jtvuong@ualberta.ca>
2022-10-02 16:53:29 -0600

    Changing attribute

--- app/src/main/java/com/example/myapplication/Dodecahedron.java
+++ app/src/main/java/com/example/myapplication/Dodecahedron.java
@@ -2,8 +2,6 @@ package com.example.myapplication;
 
 public class Dodecahedron extends Shape{
 
-    private String color = "purple";
-
     private Dodecahedron(Integer x, Integer y) {
         super(x,y);
     }
